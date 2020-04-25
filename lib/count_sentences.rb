6d2330a81755cd2b731eb336269d203d0595046c
Dhrubo Talukder <dhrubo.dh@gmail.com>
2020-04-25 01:38:13 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -8,11 +8,12 @@ class String
   end
 
   def question?
-    
+    return true if self.end_with?("?")
+    false
   end
 
   def exclamation?
-
+    return true if self.end_with?("!")
   end
 
   def count_sentences
